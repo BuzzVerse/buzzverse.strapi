0249e965bf3197ca38bcab8021b77156df2b6929
wybran <krystian.wybranowski@gmail.com>
2024-11-12 22:20:46 +0100

    fix file extension

--- src/admin/app.ts
+++ src/admin/app.ts
@@ -2,9 +2,6 @@ import type { StrapiApp } from "@strapi/strapi/admin";
 import "./global.css"
 
 export default {
-  config: {
-    locales: ["pl", "en"]
-  },
   bootstrap(app: StrapiApp) {
     console.log(app);
   }
